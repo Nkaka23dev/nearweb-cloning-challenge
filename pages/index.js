1ee2b29fa2a474d986ab16c28cb7ec220ca650e8
Nkaka23dev <nkakaeric96@gmail.com>
2022-10-31 19:08:27 +0200

    added gradient

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,21 @@ import { TagButton } from '../components/TagButton'
 export default function Home () {
   return (
     <Fragment>
-      <section className='fixed top-0 left-0 right-0 bg-white'>
+      <section className='fixed top-0 left-0 right-0 bg-white z-10'>
         <div className='max-w-[67rem] mx-auto h-24  px-2 lg:px-0 flex justify-between items-center'>
          <Link href="/nextPage">
          <div className='flex gap-5 '>
             <div className='flex items-center'>
               <span>
-                <img className='h-10' src='/svgs/sv1.svg' />
+                <img className='h-8 lg:h-10' src='/svgs/sv1.svg' />
               </span>
               <span className='ml-2 hidden md:block text-zinc-700 font-bold text-lg'>
                 Dacade
               </span>
             </div>
             <span className='border-l-2 px-5'>
-              <img className='h-10' src='/svgs/sv2.svg' />
+              <img className='h-10 hidden lg:block'  src='/svgs/sv2.svg' />
+              <img className='h-8 block lg:hidden ' src='/svgs/sv15.svg' />
             </span>
             <h1></h1>
           </div>
@@ -33,7 +34,8 @@ export default function Home () {
           </div>
         </div>
       </section>
-      <section className='max-w-5xl mx-auto h-[23rem] flex flex-col justify-end mt-40'>
+      <section className='max-w-5xl mx-auto h-[23rem] flex flex-col justify-end mt-40 relative overflow-hidden'>
+      <div className='hero-background-color -top-80 block lg:hidden absolute w-full h-96 border-b'></div>
         <div className='text-center mt-0'>
           <h1 className='lg:text-[75px] lg:w-full w-[200px] mx-auto text-[36px] text-gray-700  font-medium  '>
             Get{' '}
@@ -46,7 +48,7 @@ export default function Home () {
             Certified!
           </h1>
          
-          <h1 className='lg:text-[22px] text-lg tracking-wide  leading-[28px] text-[#676767] mt-3'>
+          <h1 className= 'max-w-md lg:max-w-full lg:text-[22px] text-lg tracking-wide leading-[28px] text-[#676767] mt-3'>
             We, in NEAR Balkans, know how important it is to have regional and
             local support. Now{' '}
             <br className='hidden lg:block text-[#3f4246]'></br> you can become
@@ -82,21 +84,23 @@ export default function Home () {
                 part of joining the NEAR community<br></br> as well as starting
                 this course.
               </span>
-              <span className='block lg:hidden'>
-                Opening a NEAR wallet is the first step and<br></br> essential
-                part of joining the NEAR community<br></br> as well as starting
+              <span className='block max-w-md lg:hidden'>
+                Opening a NEAR wallet is the first step andessential
+                part of joining the NEAR community as well as starting
                 this course.
               </span>
             </p>
             <button type="button" className="font-normal text-dark-100 text-base mt-8  border border-gray-600 px-6  focus:outline-none cursor-pointer leading-4 transition-250ms w-fit border-accent border-solid rounded-full py-3 rounded-4xl inline-block">Create wallet</button>
           </div>
+          {/* <div className='near-logo absolute -right-60 w-[70%] h-96 border-b'></div> */}
           <Image
             src='/images/image1.png'
             alt='Picture of the author'
-            className='shadow-sm'
+            className='shadow-sm '
             width={300}
             height={300}
           />
+          
         </div>
       </section>
       <section className='max-w-[67rem] mx-auto flex'>
@@ -174,7 +178,7 @@ export default function Home () {
         <div className='text-center mx-auto text-gray-700 w-full'>
           <h1 className='text-dark-100 text-center  font-medium text-2xl lg:text-4xl leading-[44px] tracking-[-0.02em]'>
             Claim NEAR certificate
-            <div class='hidden lg:block absolute top-0 -mt-[38%] left-[40%]'></div>
+            <div className='hidden lg:block absolute top-0 -mt-[38%] left-[40%]'></div>
           </h1>
           <p className='text-dark-200 font-normal text-lg leading-6 text-center'>
             <span className='hidden lg:block '>
@@ -198,7 +202,8 @@ export default function Home () {
         />
       </section>
       {/* Next Section */}
-      <section className='max-w-[60rem] mx-auto mt-32 bg-grad'>
+      <section className='max-w-[60rem] mx-auto mt-32 relative overflow-hidden'>
+        <div className='bottom-development absolute w-full h-96 border-b'></div>
         <div className='flex px-10 lg:px-0 flex-col gap-5 lg:flex-row lg:justify-between'>
           <div className='text-center lg:text-left my-auto'>
            
@@ -219,9 +224,9 @@ export default function Home () {
         </div>
       </section>
       {/*Next Section  */}
-      <section className='max-w-[50rem] mx-auto  flex py-20  flex-col gap-10'>
+      <section className='max-w-[50rem] mx-auto flex py-20  flex-col gap-10'>
         <div className='mx-auto text-lg cursor-pointer'>
-          <h1 className='text-xl'>
+          <h1 className='text-xl text-center'>
             Visit{' '}
             <span className='font-bold text-gray-700 text-center cursor-pointer'>nearbalkans.org </span>{' '}
             and make sure to follow us on our social and community channels!
@@ -229,7 +234,7 @@ export default function Home () {
         </div>
         {/* second flex */}
         <div className='mx-auto text-lg cursor-pointer'>
-          <div className='flex gap-16'>
+          <div className='flex flex-col lg:flex-row gap-16'>
 
             <div className='flex items-center'>
               <span className='border-r-2 px-16'>
@@ -291,12 +296,12 @@ export default function Home () {
           </div>
         </div>
         {/* second flex */}
-        <div className='flex justify-between text-lg cursor-pointer'>
-          <h1 className='text-center'>NEAR Balkans <br></br> 2022</h1>
+        <div className='flex flex-col lg:flex-row justify-between text-lg cursor-pointer'>
+          <h1 className='text-center'>NEAR Balkans <br className='hidden lg:block'></br> 2022</h1>
           <h1  className='text-center'>info@nearbalkans.org</h1>
-          <h1  className='text-center'>Privacy <br></br> Policy</h1>
-          <h1  className='text-center'>Cookie <br></br> Policy</h1>
-          <h1  className='text-center'>Terms & <br></br> Policy</h1>
+          <h1  className='text-center'>Privacy <br className='hidden lg:block'></br>  Policy</h1>
+          <h1  className='text-center'>Cookie <br className='hidden lg:block'></br>  Policy</h1>
+          <h1  className='text-center'>Terms & <br className='hidden lg:block'></br>  Policy</h1>
         </div>
         {/* Third flex */}
         <div className='text-lg cursor-pointer'>
